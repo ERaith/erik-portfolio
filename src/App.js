68e61c2b0ee17463d1c3af09dd3a1c95f6aea963
Erik Raith <raithhtiar@gmail.com>
2020-05-09 23:33:10 -0600

    [initialize] Adds react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Footer from './components/Footer'
+import Card from './components/Projects'
+import './App.scss'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Card/>
+        <Footer/>
+      </div>
+    )
+  }
 }
-
-export default App;
