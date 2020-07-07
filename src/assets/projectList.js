7f13428a1bd43f15822289ff299f0296874de99b
ERaith <raithhtiar@gmail.com>
2020-07-06 20:40:36 -0600

    Update projectList.js

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -10,23 +10,37 @@ export const projectList = {
       description: {
         about: {
           text:
-            "Big Dogs Huge Paws is an adoption website for a non-profit organization. I began volunteering when their previous website crashed. My primary responsibilty is to work on the frontend, in Angular/TS.",
+            "Big Dogs Huge Paws is a website used by the Adoption rescue based in Denver Colorado. I had the opportunity to volunteer time as a FullStack Developer on their new website. My position and job was to improve the UX on mobile devices and the developer tools available to the team. Our overall team objective was to take an existing website that crashed completely and spin up a new website ",
           url:
             "https://user-images.githubusercontent.com/6379500/86557047-3f44a000-bf12-11ea-99d4-45b40a1f4418.png",
         },
 
         challenges: {
-          text: [],
+          text: [
+         "Improve Mobile ",
+"Work on coding in a professional setting",
+"Learn GraphQL integration with Strapi",
+"Use Apollo to connect relationships between GraphQL and Angular",
+"Develop my understanding of Javascript using a Superscript(TypeScript)",
+
+          ],
           url:
             "https://user-images.githubusercontent.com/6379500/86557045-3e137300-bf12-11ea-8d4b-2c0c19701079.png",
         },
         wins: {
-          text: [],
+          text: "Jumping into a project with two other veteran software developers was intimidating at first especially when working with TypeScript for the first time. It took a bit to get comfortable with using Interfaces, but when I did it was magical. Every single portion of the code now explicitly stated what type is expected and what type is returned.  ",
           url:
             "https://user-images.githubusercontent.com/6379500/86557042-3b188280-bf12-11ea-8b69-7d612b202696.png",
         },
         stack: "stack description",
-        stackBadges: ["Angular", "Strapi", "Docker", "TypeScript", "Apollo"],
+        stackBadges: ["Strapi",
+"Docker",
+"Angular",
+"TypeScript",
+"Swiper",
+"GraphQL",
+"Apollo",
+],
       },
     },
     {
@@ -39,51 +53,88 @@ export const projectList = {
       description: {
         about: {
           text:
-            "This project is a pokemon builder! The app allows the user to choose which pokemon they want to have on their team and compare their defensive weaknesses to build a devistating team for competition. The user can sort the pokemon by name or type and save their chosen team for later.",
+            "Root directory is a plant management application that allows the user to keep track of their watering dates, changes of locations, and overall keep their plant healthy. We use Plant Ai api to determine the type of plant uploaded and use this to confirm the identity of said plant. We provide alerts for the user based on upcoming watering dates. In the future, we are looking to add users, plant care suggestions based on user contributions.  We use MongoDB and Flask the backend and Express/Angular for our frontend.",
           url:
             "https://user-images.githubusercontent.com/6379500/86557421-4cae5a00-bf13-11ea-967b-07c0d065c4b2.png",
         },
 
         challenges: {
-          text: [],
+          text: [
+"          Learn Python, Flask, and  MongoDB as a BE Dev",
+"Write clean code in Angular using Typescript",
+"Implement NGRX in Angular",
+"Use Bootstrap and SCSS to get the hang of things. :)",
+
+          
+          ],
           url:
             "https://user-images.githubusercontent.com/6379500/86557052-410e6380-bf12-11ea-97e7-e674ff0e6584.png",
         },
         wins: {
-          text: [],
+          text: "This project won 3rd place at the Turing School of Design Demo Competition!  I loved hearing the great feedback on future iterations of the app and the app is still in progress. Next iteration: Users! I had a blast working with our group and learning Observables, subscribers, marble diagrams over the course of this project, and am excited to see where it goes. ",
           url:
             "https://user-images.githubusercontent.com/6379500/86557055-423f9080-bf12-11ea-9fa9-a2d00c49c364.png",
         },
         stack: "stack description",
-        stackBadges: ["Angular", "Strapi", "Docker", "TypeScript", "Apollo"],
+        stackBadges: [
+ "       MongoDB",
+"Python",
+"AWS S3",
+"Unittest",
+'TravisCi",
+"Angular",
+"Jasmine",
+"Kharma",
+"Express",
+],
       },
     },
     {
       title: "Pokemon Team Builder",
       routename: "PokemonTeamBuilder",
+      liveSite:"https://secret-sierra-69834.herokuapp.com/pokedex/team/team-rocket",
       githuburl: "https://github.com/ERaith/pmt",
       imageurl:
         "https://user-images.githubusercontent.com/6379500/86557049-410e6380-bf12-11ea-9924-fed47e5f9072.png",
       description: {
         about: {
           text:
-            "This project is a pokemon builder! The app allows the user to choose which pokemon they want to have on their team and compare their defensive weaknesses to build a devistating team for competition. The user can sort the pokemon by name or type and save their chosen team for later.",
+            "The app allows the user to choose which pokemon they want to have on their team and compare their defensive weaknesses to build a devastating team for competition. The user can sort the pokemon by name or type and save their chosen team for later. Local storage is used at the moment, however, a backend is coming soon!",
           url:
             "https://user-images.githubusercontent.com/6379500/86557049-410e6380-bf12-11ea-9924-fed47e5f9072.png",
         },
 
         challenges: {
-          text: [],
+          text: [
+"Write squeaky clean, well-refactored code using ES6 syntax.",
+"Make informed design decisions to create a user-friendly application.",
+"Keep state-based components to a minimum and leverage more functional components.",
+"Use a modular architecture for your application file structure.",
+"Think deeply about React Lifecycle Methods.",
+"Use propTypes for every component receiving props.",
+"Write tests for React components and asynchronous functionality.",
+"Utilize Redux for providing props from a unified prop store.",
+"Write full coverage tests for Redux actions and reducers.",
+
+          ],
           url:
             "https://user-images.githubusercontent.com/6379500/86557048-4075cd00-bf12-11ea-86d7-aea5f809b25b.png",
         },
         wins: {
-          text: "I loved working with React-router and redux on this project. This was the first time",
+          text: "What can I say? Its a pokemon pet project. It felt fun while I was making it and I still eagerly await updating it with Hooks, getting user login implemented, adding more statistics, and creating a simulated battle mode.",
           url:
             "https://user-images.githubusercontent.com/6379500/86557050-410e6380-bf12-11ea-853f-4e14579e1fa5.png",
         },
         stack: "stack description",
-        stackBadges: ["Angular", "Strapi", "Docker", "TypeScript", "Apollo"],
+        stackBadges: [
+ "       JavaScript",
+"SASS",
+"Redux",
+"React Router",
+"Jest ",
+"React Testing Library",
+
+        ],
       },
     },
     {
@@ -117,7 +168,7 @@ export const projectList = {
             "https://user-images.githubusercontent.com/6379500/79268460-0c7d9500-7e58-11ea-8f3b-80574dbbbfb6.png",
         },
         wins: {
-          text: "I loved working with React-router and redux on this project. This was the first time I used redux instead of prop drilling and it was fantastic!",
+          text: "This project utilized Redux and it changed my life. I love prop drilling as much as the next Dev, but having a global store is fantastic! The next iteration would be to transition over to React Hooks. I had a ton of fun working with this team and adhering to a strict agile workflow(as dry as that sounds). ",
           url:
             "https://user-images.githubusercontent.com/6379500/79268559-31720800-7e58-11ea-8a56-7f7036e8ad34.png",
         },
@@ -140,18 +191,31 @@ export const projectList = {
       description: {
         about: {
           text:
-            "This project is a pokemon builder! The app allows the user to choose which pokemon they want to have on their team and compare their defensive weaknesses to build a devistating team for competition. The user can sort the pokemon by name or type and save their chosen team for later.",
+            "This project was bootstrapped with Create React App. It is a clone of airBnB where the user can view different areas of Denver and view house listings available for rent. The user has the option to favorite individual listings and refer back to those at a later date.",
           url:
             "https://user-images.githubusercontent.com/6379500/86557421-4cae5a00-bf13-11ea-967b-07c0d065c4b2.png",
         },
 
         challenges: {
-          text: [],
+          text: [
+          
+"Write squeaky clean, well-refactored code using ES6 syntax.",
+"Make informed design decisions to create a user-friendly application.",
+"Keep state-based components to a minimum and leverage more functional components.",
+"Use a modular architecture for your application file structure.",
+"Think deeply about React Lifecycle Methods.",
+"Become familiar with promises, nested fetch requests, and handling the UI based on acceptance of data.",
+"Become familiar with routing and how to handle dynamic routes.",
+"Use propTypes for every component receiving props.",
+"Write tests for React components and some asynchronous functionality.",
+"Create an app relying on clean code practices",
+
+          ],
           url:
             "https://user-images.githubusercontent.com/6379500/86557052-410e6380-bf12-11ea-97e7-e674ff0e6584.png",
         },
         wins: {
-          text: "",
+          text: "One of our earlier projects, but the first time we worked in React. This project relies on prop drilling and keeps the user data stored in Local Storage. I thoroughly enjoyed working with React Router and had a blast creating a little breadcrumb component from scratch!  ",
           url:
             "https://user-images.githubusercontent.com/6379500/86557055-423f9080-bf12-11ea-9fa9-a2d00c49c364.png",
         },
